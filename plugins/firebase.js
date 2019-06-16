52f439cfd6ea14dac8479baf4085580d1e78f8d3
taishikato <taishi.k0903@gmail.com>
2019-06-15 20:08:41 -0700

    :zap: Change Firebase project

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: 'AIzaSyBSqqSE8vAV2P04PGl4Dtz6zhc2sL6qo88',
-    authDomain: 'hangout-map.firebaseapp.com',
-    databaseURL: 'https://hangout-map.firebaseio.com',
-    projectId: 'hangout-map',
-    storageBucket: 'hangout-map.appspot.com',
-    messagingSenderId: '49021875088',
-    appId: '1:49021875088:web:cbeaa73a32f3e0d9'
+    apiKey: 'AIzaSyCtxikc-wFo3UesWkXyHDS4GHrCmvOBsro',
+    authDomain: 'nomadplacemap.firebaseapp.com',
+    databaseURL: 'https://nomadplacemap.firebaseio.com',
+    projectId: 'nomadplacemap',
+    storageBucket: 'nomadplacemap.appspot.com',
+    messagingSenderId: '574266788442',
+    appId: '1:574266788442:web:2cd09d473381e6d3'
   })
 }
 export default firebase
